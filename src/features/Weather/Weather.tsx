f29274a651dacda3e5fd190d19d882ebd669e5f2
Midas <42305420+A11eged@users.noreply.github.com>
2023-02-19 22:35:01 -0500

    create async thunk

--- src/features/Weather/Weather.tsx
+++ src/features/Weather/Weather.tsx
@@ -1,72 +1,83 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { WeatherType } from '../../app/models';
-import {
-  setBase,
-  setClouds,
-  setCod,
-  setCoord,
-  setName,
-  setDt,
-  setId,
-  setMain,
-  setSys,
-  setTimezone,
-  setVisibility,
-  setWind,
-  selectWeather,
-} from './WeatherSlice';
-import {
-  getEndpointZipcode,
-  getDataOneMove,
-  plain_endpoint,
-} from './WeatherFetch';
-import { selectName, selectZip } from '../Location/LocationSlice';
-import { WeatherCard } from './WeatherCard';
+import { addWeather } from './WeatherSlice';
 
 export const Weather = () => {
-  const dispatch = useAppDispatch();
-  const weatherSelector = useAppSelector(selectWeather);
-  const zipcodeSelector = useAppSelector(selectZip);
-  const { data } = getDataOneMove<WeatherType>(
-    getEndpointZipcode(plain_endpoint, zipcodeSelector)
-  );
-  const dispatchWeather = () => {
-    dispatch(setBase(data ? data.base : 'none'));
-    dispatch(setClouds(data ? data.clouds.all : 0));
-    dispatch(setCod(data ? data.cod : 0));
-    dispatch(setCoord(data ? data.coord : { lon: 0, lat: 0 }));
-    dispatch(setName(data ? data.name : 'none'));
-    dispatch(setDt(data ? data.dt : ''));
-    dispatch(setId(data ? data.id : 0));
-    dispatch(
-      setMain(
-        data
-          ? data.main
-          : {
-              temp: 0,
-              feels_like: 0,
-              temp_min: 0,
-              temp_max: 0,
-              pressure: 0,
-              humidity: 0,
-            }
-      )
-    );
-    dispatch(
-      setSys(
-        data
-          ? data.sys
-          : { type: 0, id: 0, country: 'none', sunrise: 0, sunset: 0 }
-      )
-    );
-    dispatch(setTimezone(data ? data.timezone : 0));
-    dispatch(setVisibility(data ? data.visibility : 0));
-    dispatch(setWind(data ? data.wind.speed : 0));
-  };
+  console.log(addWeather());
   return (
     <div>
-      <WeatherCard />
+      <button>Test</button>
     </div>
   );
 };
+
+// import {
+//   setBase,
+//   setClouds,
+//   setCod,
+//   setCoord,
+//   setName,
+//   setDt,
+//   setId,
+//   setMain,
+//   setSys,
+//   setTimezone,
+//   setVisibility,
+//   setWind,
+//   selectWeather,
+// } from './WeatherSlice';
+// import {
+//   getEndpointZipcode,
+//   getDataOneMove,
+//   plain_endpoint,
+// } from './WeatherFetch';
+// import { selectName, selectZip } from '../Location/LocationSlice';
+// import { WeatherCard } from './WeatherCard';
+
+// export const Weather = () => {
+//   const dispatch = useAppDispatch();
+//   const weatherSelector = useAppSelector(selectWeather);
+//   const zipcodeSelector = useAppSelector(selectZip);
+//   const { data } = getDataOneMove<WeatherType>(
+//     getEndpointZipcode(plain_endpoint, zipcodeSelector)
+//   );
+//   const dispatchWeather = () => {
+//     dispatch(setBase(data ? data.base : 'none'));
+//     dispatch(setClouds(data ? data.clouds.all : 0));
+//     dispatch(setCod(data ? data.cod : 0));
+//     dispatch(setCoord(data ? data.coord : { lon: 0, lat: 0 }));
+//     dispatch(setName(data ? data.name : 'none'));
+//     dispatch(setDt(data ? data.dt : ''));
+//     dispatch(setId(data ? data.id : 0));
+//     dispatch(
+//       setMain(
+//         data
+//           ? data.main
+//           : {
+//               temp: 0,
+//               feels_like: 0,
+//               temp_min: 0,
+//               temp_max: 0,
+//               pressure: 0,
+//               humidity: 0,
+//             }
+//       )
+//     );
+//     dispatch(
+//       setSys(
+//         data
+//           ? data.sys
+//           : { type: 0, id: 0, country: 'none', sunrise: 0, sunset: 0 }
+//       )
+//     );
+//     dispatch(setTimezone(data ? data.timezone : 0));
+//     dispatch(setVisibility(data ? data.visibility : 0));
+//     dispatch(setWind(data ? data.wind.speed : 0));
+//   };
+//   return (
+//     <div>
+//       <WeatherCard />
+//     </div>
+//   );
+// };
--- src/features/Weather/WeatherSlice.ts
+++ src/features/Weather/WeatherSlice.ts
@@ -1,121 +1,185 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { WeatherType } from '../../app/models';
 import { RootState } from '../../app/store';
+import { key } from '../../app/constants';
+import {
+  plain_endpoint,
+  coordinate_endpoint,
+  getEndpointZipcode,
+  getEndpointCoordinates,
+} from '../Weather/WeatherFetchHelpers';
+import { useAppSelector } from '../../app/hooks';
 
-const initialState: WeatherType = {
-  base: '',
-  clouds: { all: 0 },
-  cod: 0,
-  coord: { lon: 0, lat: 0 },
-  name: '',
-  dt: '',
-  id: 0,
-  main: {
-    feels_like: 0,
-    temp: 0,
-    humidity: 0,
-    temp_max: 0,
-    temp_min: 0,
-    pressure: 0,
-  },
-  sys: {
-    country: '',
-    id: 0,
-    sunrise: 0,
-    sunset: 0,
-    type: 0,
-  },
-  timezone: 0,
-  visibility: 0,
-  wind: { speed: 0 },
+enum Status {
+  idle = 'idle',
+  loading = 'loading',
+  failed = 'failed',
+  success = 'success',
+}
+
+export type RequestState = {
+  value: WeatherType;
+  errors: string[];
+  status: Status;
 };
+
+// Take Weather Object and return a Promise that resolves to Weather Object
+export const addWeather = createAsyncThunk('weather/addWeather', () => {
+  const zipSelector = useAppSelector(
+    (state: RootState) => state.location.zipcode
+  );
+  return fetch(getEndpointZipcode(plain_endpoint, zipSelector), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data);
+});
 
 export const weatherSlice = createSlice({
   name: 'weather',
-  initialState: initialState,
+  initialState: {
+    value: [] as WeatherType[],
+    errors: [] as string[],
+    status: Status.idle,
+  },
   reducers: {
-    setBase: (state, action: PayloadAction<string>) => {
-      state.base = action.payload;
+    logWeather(state) {
+      state.value = [];
     },
-    setClouds: (state, action: PayloadAction<number>) => {
-      state.clouds.all = action.payload;
-    },
-    setCod: (state, action: PayloadAction<number>) => {
-      state.cod = action.payload;
-    },
-    setCoord: (state, action: PayloadAction<{ lon: number; lat: number }>) => {
-      state.coord.lon = action.payload.lon;
-      state.coord.lat = action.payload.lat;
-    },
-    setName: (state, action: PayloadAction<string>) => {
-      state.name = action.payload;
-    },
-    setDt: (state, action: PayloadAction<string>) => {
-      state.dt = action.payload;
-    },
-    setId: (state, action: PayloadAction<number>) => {
-      state.id = action.payload;
-    },
-    setMain: (
-      state,
-      action: PayloadAction<{
-        feels_like: number;
-        temp: number;
-        humidity: number;
-        temp_max: number;
-        temp_min: number;
-        pressure: number;
-      }>
-    ) => {
-      state.main.feels_like = action.payload.feels_like;
-      state.main.temp = action.payload.temp;
-      state.main.humidity = action.payload.humidity;
-      state.main.temp_max = action.payload.temp_max;
-      state.main.temp_min = action.payload.temp_min;
-      state.main.pressure = action.payload.pressure;
-    },
-    setSys: (
-      state,
-      action: PayloadAction<{
-        country: string;
-        id: number;
-        sunrise: number;
-        sunset: number;
-        type: number;
-      }>
-    ) => {
-      state.sys.country = action.payload.country;
-      state.sys.id = action.payload.id;
-      state.sys.sunrise = action.payload.sunrise;
-      state.sys.sunset = action.payload.sunset;
-      state.sys.type = action.payload.type;
-    },
-    setTimezone: (state, action: PayloadAction<number>) => {
-      state.timezone = action.payload;
-    },
-    setVisibility: (state, action: PayloadAction<number>) => {
-      state.visibility = action.payload;
-    },
-    setWind: (state, action: PayloadAction<number>) => {
-      state.wind.speed = action.payload;
-    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(addWeather.pending, (state) => {
+      state.status = Status.loading;
+    });
+    builder.addCase(addWeather.fulfilled, (state, action) => {
+      state.status = Status.success;
+      state.value = action.payload;
+    });
+    builder.addCase(addWeather.rejected, (state, action) => {
+      state.status = Status.failed;
+      state.errors.push('Error');
+    });
   },
 });
 
-export const {
-  setBase,
-  setClouds,
-  setCod,
-  setCoord,
-  setName,
-  setDt,
-  setId,
-  setMain,
-  setSys,
-  setTimezone,
-  setVisibility,
-  setWind,
-} = weatherSlice.actions;
-export const selectWeather = (state: RootState) => state.weather;
-
+export const { logWeather } = weatherSlice.actions;
 export default weatherSlice.reducer;
+
+// const initialState: WeatherType = {
+//   base: '',
+//   clouds: { all: 0 },
+//   cod: 0,
+//   coord: { lon: 0, lat: 0 },
+//   name: '',
+//   dt: '',
+//   id: 0,
+//   main: {
+//     feels_like: 0,
+//     temp: 0,
+//     humidity: 0,
+//     temp_max: 0,
+//     temp_min: 0,
+//     pressure: 0,
+//   },
+//   sys: {
+//     country: '',
+//     id: 0,
+//     sunrise: 0,
+//     sunset: 0,
+//     type: 0,
+//   },
+//   timezone: 0,
+//   visibility: 0,
+//   wind: { speed: 0 },
+// };
+
+// export const weatherSlice = createSlice({
+//   name: 'weather',
+//   initialState: initialState,
+//   reducers: {
+//     setBase: (state, action: PayloadAction<string>) => {
+//       state.base = action.payload;
+//     },
+//     setClouds: (state, action: PayloadAction<number>) => {
+//       state.clouds.all = action.payload;
+//     },
+//     setCod: (state, action: PayloadAction<number>) => {
+//       state.cod = action.payload;
+//     },
+//     setCoord: (state, action: PayloadAction<{ lon: number; lat: number }>) => {
+//       state.coord.lon = action.payload.lon;
+//       state.coord.lat = action.payload.lat;
+//     },
+//     setName: (state, action: PayloadAction<string>) => {
+//       state.name = action.payload;
+//     },
+//     setDt: (state, action: PayloadAction<string>) => {
+//       state.dt = action.payload;
+//     },
+//     setId: (state, action: PayloadAction<number>) => {
+//       state.id = action.payload;
+//     },
+//     setMain: (
+//       state,
+//       action: PayloadAction<{
+//         feels_like: number;
+//         temp: number;
+//         humidity: number;
+//         temp_max: number;
+//         temp_min: number;
+//         pressure: number;
+//       }>
+//     ) => {
+//       state.main.feels_like = action.payload.feels_like;
+//       state.main.temp = action.payload.temp;
+//       state.main.humidity = action.payload.humidity;
+//       state.main.temp_max = action.payload.temp_max;
+//       state.main.temp_min = action.payload.temp_min;
+//       state.main.pressure = action.payload.pressure;
+//     },
+//     setSys: (
+//       state,
+//       action: PayloadAction<{
+//         country: string;
+//         id: number;
+//         sunrise: number;
+//         sunset: number;
+//         type: number;
+//       }>
+//     ) => {
+//       state.sys.country = action.payload.country;
+//       state.sys.id = action.payload.id;
+//       state.sys.sunrise = action.payload.sunrise;
+//       state.sys.sunset = action.payload.sunset;
+//       state.sys.type = action.payload.type;
+//     },
+//     setTimezone: (state, action: PayloadAction<number>) => {
+//       state.timezone = action.payload;
+//     },
+//     setVisibility: (state, action: PayloadAction<number>) => {
+//       state.visibility = action.payload;
+//     },
+//     setWind: (state, action: PayloadAction<number>) => {
+//       state.wind.speed = action.payload;
+//     },
+//   },
+// });
+
+// export const {
+//   setBase,
+//   setClouds,
+//   setCod,
+//   setCoord,
+//   setName,
+//   setDt,
+//   setId,
+//   setMain,
+//   setSys,
+//   setTimezone,
+//   setVisibility,
+//   setWind,
+// } = weatherSlice.actions;
+// export const selectWeather = (state: RootState) => state.weather;
